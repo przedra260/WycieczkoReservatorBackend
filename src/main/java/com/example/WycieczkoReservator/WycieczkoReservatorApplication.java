340fa59d8f68b7809a112d0ba6d709391755427f
przedra260 <pdrazkiewicz96@gmail.com>
2021-03-21 19:50:44 +0100

    app template

---
+++ src/main/java/com/example/WycieczkoReservator/WycieczkoReservatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.WycieczkoReservator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WycieczkoReservatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WycieczkoReservatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WycieczkoReservator/WycieczkoReservatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WycieczkoReservator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WycieczkoReservatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
