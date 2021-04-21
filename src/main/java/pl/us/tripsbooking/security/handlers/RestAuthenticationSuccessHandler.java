a463c1b079425cf6cc4b9e80955d7feadf51e986
Sarchael <mpochec2012@gmail.com>
2021-04-21 23:34:34 +0200

    Returning role in login reponse

--- src/main/java/pl/us/tripsbooking/security/handlers/RestAuthenticationSuccessHandler.java
+++ src/main/java/pl/us/tripsbooking/security/handlers/RestAuthenticationSuccessHandler.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import pl.us.tripsbooking.security.model.CustomSecurityUser;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,12 +26,13 @@ public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-        UserDetails principal = (UserDetails) authentication.getPrincipal();
+        CustomSecurityUser principal = (CustomSecurityUser) authentication.getPrincipal();
         //Tu można dodać własny serwis z pobieraniem customowych informacji usera i wrzucenie calosci jako Subject do tokena
         String token = JWT.create()
                 .withSubject(principal.getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
                 .sign(Algorithm.HMAC256(secret));
-        response.getOutputStream().print("{\"token\": \"" + token + "\"}");
+        response.getOutputStream().print("{\"token\": \"" + token + "\", " +
+                                          "\"role\": \"" + principal.getAuthorities().stream().findFirst().get() + "\"}");
     }
 }
